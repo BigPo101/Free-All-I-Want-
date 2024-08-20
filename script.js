7d2c088f77deb96799e49c1b880bee82dcfdef6d
BigPo101 <123412376+BigPo101@users.noreply.github.com>
2024-08-20 11:58:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,13 @@
-function playMedia(src, title) {
-    const mediaPlayer = document.getElementById('mediaPlayer');
-    const mediaSource = document.getElementById('mediaSource');
-    const mediaTitle = document.getElementById('mediaTitle');
-
-    mediaSource.src = src;
-    mediaTitle.textContent = title;
-    mediaPlayer.load();
-    mediaPlayer.play();
-}
-
 document.addEventListener("DOMContentLoaded", function() {
     var introVideo = document.getElementById('introVideo');
     var introVideoContainer = document.getElementById('introVideoContainer');
     var mainContent = document.getElementById('mainContent');
 
     introVideo.onended = function() {
-        // Fade out the intro video with a scale-down effect
+        // Instantly hide the intro video
         introVideoContainer.classList.add('hidden');
         
-        // Show the main content with a delay to match the fade-out
-        setTimeout(function() {
-            mainContent.classList.remove('hidden');
-            mainContent.classList.add('show');
-        }, 1000); // Time to match the fade-out duration
+        // Instantly show the main content
+        mainContent.classList.remove('hidden');
     };
 });
